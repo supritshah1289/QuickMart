8b62a9ce4f7fbd3d467f974cf7574d801142cb88
supritkumar shah <suprit7@gmail.com>
2017-12-28 12:31:27 -0500

    header footer and css fix add

--- Gemfile
+++ Gemfile
@@ -5,8 +5,11 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-ruby "2.4.1"
+#css framework
+gem 'bootstrap', '~> 4.0.0.alpha3'
+gem 'jquery-rails'
 
+ruby "2.4.2"
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
 # Use Puma as the app server
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/categories' => 'categories#index'
+  root 'categories#index'
   get '/categories/:id' => 'categories#show', as: :category
   get '/categories_new' => 'categories#new'
   post'/categories' => 'categories#create'
